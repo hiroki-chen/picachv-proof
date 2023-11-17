6ed34a96ef45fc54c2b1b3245e7e7fd960523fe1
Hiroki Chen <etyyuiope@qq.com>
2023-11-16 21:21:33 -0500

    Add more proofs

--- policy_proof/coq/policy.v
+++
@@ -1,8 +0,0 @@
-(* The policy library. *)
-
-(* For now, the policy is just a placeholder. *)
-Inductive policy : Set :=
-  | policy_empty : policy
-  | policy_select: policy
-  | policy_top : policy
-  .
