112c0c3bd3d4ee2b633307e2b96d4067b1f4db03
Hiroki Chen <etyyuiope@qq.com>
2023-11-18 21:47:44 -0500

    proof: add ordering for char type

--- policy_proof/coq/ordering.v
+++ policy_proof/coq/ordering.v
@@ -3,6 +3,8 @@ Require Import SetoidClass.
 Require Import OrderedType.
 Require Import Equivalence.
 Require Import Lia.
+Require Import Ascii.
+Require Import String.
 Require Import Compare_dec.
 
 Class Ordered (A: Set) := {
@@ -13,7 +15,6 @@ Class Ordered (A: Set) := {
   neq: forall lhs rhs: A, lt lhs rhs -> lhs =/= rhs;
   cmp: forall lhs rhs: A, Compare lt equiv lhs rhs;
 }.
-
 
 Definition nat_dec : forall (a b: nat), {a < b} + {a = b} + {b < a}.
  intros. pose (lt_eq_lt_dec a b).
@@ -63,3 +64,47 @@ refine (
     + apply LT. unfold bool_lt. auto.
     + apply EQ. unfold equiv. auto.
 Defined.
+
+Definition to_lower (a' : ascii) : ascii :=
+  let a := nat_of_ascii a' in
+  if le_lt_dec a (nat_of_ascii "z"%char) then
+    if le_lt_dec (nat_of_ascii "a"%char) a then
+      ascii_of_nat (a - nat_of_ascii "a"%char + nat_of_ascii "A")
+    else a'
+  else a'.
+
+Definition char_eq (lhs rhs: ascii): Prop :=
+  (nat_of_ascii (to_lower lhs)) = (nat_of_ascii (to_lower rhs)).
+Definition char_lt (lhs rhs: ascii): Prop :=
+  (nat_of_ascii (to_lower lhs)) < (nat_of_ascii (to_lower rhs)).
+Definition char_trans: Transitive char_lt.
+  unfold Transitive.
+  intros. unfold char_lt in *. lia.
+Defined.
+
+Instance char_eq_setoid : Setoid ascii.
+refine (@Build_Setoid _ char_eq _).
+  econstructor.
+  - unfold Reflexive. intros. unfold char_eq. auto.
+  - unfold Symmetric. intros. unfold char_eq in *. auto.
+  - unfold Transitive. intros. unfold char_eq in *. lia.
+Defined.
+
+Global Instance char_ordered: Ordered ascii.
+refine (
+  @Build_Ordered ascii char_eq_setoid char_lt char_trans _ _
+).
+  intros. simpl.
+  - unfold char_lt in H. unfold complement. intros.
+    rewrite H0 in H. lia.
+    (* Directly destrucing on ascii itself is not doable. *)
+  - intros. destruct (nat_dec (nat_of_ascii (to_lower lhs)) (nat_of_ascii (to_lower rhs))).
+    + destruct s.
+      * refine (LT _ ). unfold char_lt. auto.
+      * refine (EQ _ ). unfold equiv. simpl. unfold char_eq. auto.
+    + refine (GT _ ). unfold char_lt. auto.
+Defined.
+
+Definition string_eq (lhs rhs: string): Prop := lhs = rhs.
+Definition string_lt (lhs rhs: string): Prop :=
+  String.compare lhs rhs = Lt.
